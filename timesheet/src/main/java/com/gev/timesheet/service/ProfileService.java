d591241349b1eb39b54f2c0fba6f01239c277e99
George Wang <georgewang2048@gmail.com>
2020-12-17 22:09:57 -0500

    Backup changes.

--- timesheet/src/main/java/com/gev/timesheet/service/ProfileService.java
+++ timesheet/src/main/java/com/gev/timesheet/service/ProfileService.java
@@ -3,12 +3,12 @@ package com.gev.timesheet.service;
 import com.gev.timesheet.dao.EmployeeDAO;
 import com.gev.timesheet.domain.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Component
+@Service
 public class ProfileService {
     private EmployeeDAO employeeDAO;
 
